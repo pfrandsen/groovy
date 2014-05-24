1018c1f5b8fd8d4abd46b7514ff436729719f3b3
Peter Frandsen <pfrandsen@gmail.com>
2014-05-24 19:42:19 +0200

    Added comment

--- src/filesystem/util/BetaSchemaCleanup.groovy
+++ src/filesystem/util/BetaSchemaCleanup.groovy
@@ -1,4 +1,9 @@
 package filesystem.util
 
+/**
+ * This class is used to compare two directory hierarchies, a "master" and a "clone".
+ * It will remove all the files from clone that is in master and where the file content is identical.
+ * After all the identical files have been removed, all the empty folders in clone will be removed
+ */
 class CompareAndCleanup {
 }
